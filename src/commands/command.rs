4bf410d32befd7e9c8685249835d382fef303c94
bogzbonny <bogzbonny@protonmail.com>
2025-02-20 02:44:46 +0000

    [kwaak]: Committed changes after completion

--- src/commands/command.rs
+++ src/commands/command.rs
@@ -38,6 +38,9 @@ pub enum Command {
     /// Retry the last chat with the agent
     /// Will reset history to the point of the last chat, then re-run the chat
     RetryChat,
+
+    /// Fetch and summarize a GitHub issue and its comments
+    GithubIssue { number: u64 },
 }
 
 #[derive(Debug, Clone, Builder)]
